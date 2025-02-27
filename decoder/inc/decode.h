2a94504b9102fe2143a63374ed2e05e6a6e3664d
Fisher Jepsen <efjepsen@mit.edu>
2025-02-27 10:42:28 -0500

    Use global timestamp tracker across all channels, instead of per-channel
    Previously we observed SR3 for each channel, which was a misunderstanding of the requirements.

--- decoder/inc/decode.h
+++ decoder/inc/decode.h
@@ -5,16 +5,9 @@
 #include <stdbool.h>
 #include "subscribe.h"
 
-#define NUM_MAX_CHANNELS NUM_MAX_SUBSCRIPTIONS + 1
 #define MAX_FRAME_SIZE 64
 
 #pragma pack(push, 1)
-
-typedef struct {
-    bool active;
-    channel_id_t channel;
-    timestamp_t timestamp;
-} last_timestamp_t;
 
 typedef union {
     uint8_t data[MAX_FRAME_SIZE];
--- decoder/src/decode.c
+++ decoder/src/decode.c
@@ -4,35 +4,12 @@
 #include "cryptosystem.h"
 
 extern const kdf_node_t SUB0_NODE;
-static last_timestamp_t last_timestamps[NUM_MAX_CHANNELS] = { 0 };
 
-/** @brief Locate the last timestamp entry for a given channel
- * 
- *  @param channel: uint32_t, channel number of the timestamp entry to find
- * 
- *  @return last_timestamp_t *: pointer to the last timestamp entry OR an unused entry, NULL if not found
- */
-last_timestamp_t * find_last_timestamp(uint32_t channel) {
-    last_timestamp_t * entry;
-
-    // Look for an active entry
-    for (int i = 0; i < NUM_MAX_CHANNELS; i++) {
-        entry = &last_timestamps[i];
-        if (entry->active && entry->channel == channel) {
-            return entry;
-        }
-    }
-
-    // Find an open slot
-    for (int i = 0; i < NUM_MAX_CHANNELS; i++) {
-        entry = &last_timestamps[i];
-        if (!entry->active) {
-            return entry;
-        }
-    }
-
-    return NULL;
-}
+// Use a bool to track if we've decoded any frame yet,
+// so as to not assume magic values of last_timestamp being OK
+// to ignore SR3.
+static bool decoded_anything = false;
+static timestamp_t last_timestamp = 0;
 
 /** @brief Handle decode command, returning a successfully decoded frame over UART
  * 
@@ -53,42 +30,36 @@ void decode(packet_t * packet, uint16_t len) {
 
     if (subscription != NULL || enc_frame->channel == 0) {
         // Check timestamp
-        last_timestamp_t * entry = find_last_timestamp(enc_frame->channel);
-
-        if (entry != NULL) {
-            if ((entry->active && enc_frame->timestamp > entry->timestamp) || (!entry->active)) {
-                // Find the correct decryption key
-                kdf_node_t * kdf_node = &SUB0_NODE;
-                if (enc_frame->channel != 0) {
-                    kdf_node = find_ts_parent(subscription, enc_frame->timestamp);
-                    if (kdf_node == NULL) {
-                        send_error();
-                        return;
-                    }
-                }
-
-                aeskey_t frame_key = { 0 };
-                int ret = derive_node_subkey(kdf_node, enc_frame->timestamp, &frame_key);
-                if (ret != 0) {
+        if ((decoded_anything == false) || (enc_frame->timestamp > last_timestamp)) {
+            // Find the correct decryption key
+            kdf_node_t * kdf_node = &SUB0_NODE;
+            if (enc_frame->channel != 0) {
+                kdf_node = find_ts_parent(subscription, enc_frame->timestamp);
+                if (kdf_node == NULL) {
                     send_error();
                     return;
                 }
+            }
 
-                // Decrypt
-                uint16_t frame_len = 0;
-                frame_t * frame = decrypt_frame(packet, len, &frame_key, &frame_len);
+            aeskey_t frame_key = { 0 };
+            int ret = derive_node_subkey(kdf_node, enc_frame->timestamp, &frame_key);
+            if (ret != 0) {
+                send_error();
+                return;
+            }
 
-                // Send the frame
-                if (frame != NULL && frame_len > 0 && frame_len <= MAX_FRAME_SIZE) {
-                    if (send_packet(frame->data, frame_len, OPCODE_DECODE)) {
-                        // For a successful decryption, update the entry
-                        entry->active = true;
-                        entry->channel = enc_frame->channel;
-                        entry->timestamp = enc_frame->timestamp;
-    
-                        return;
-                    }
-                }
+            // Decrypt
+            uint16_t frame_len = 0;
+            frame_t * frame = decrypt_frame(packet, len, &frame_key, &frame_len);
+
+            // Send the frame
+            if (frame != NULL && frame_len > 0 && frame_len <= MAX_FRAME_SIZE) {
+                // For a successful decryption, update last_timestamp.
+                decoded_anything = true;
+                last_timestamp = enc_frame->timestamp;
+
+                send_packet(frame->data, frame_len, OPCODE_DECODE);
+                return;
             }
         }
     }
