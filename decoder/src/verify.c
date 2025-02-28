872f77dddd742683e59d6fb2fd05db5b9c367081
Fisher Jepsen <efjepsen@mit.edu>
2025-02-28 00:07:28 -0500

    Add comments documenting some oustanding functions in verify.c

--- decoder/src/verify.c
+++ decoder/src/verify.c
@@ -3,6 +3,10 @@
 extern const uint8_t SK_BYTES[32];
 static ed25519_key signing_key = {0};
 
+/** @brief Initialize wolfcrypt ed25519 signing key object.
+ * 
+ *  @return int: 0 on success, otherwise a wolfcrypt error code.
+ */
 int init_signing_key(void) {
     int ret = wc_ed25519_init(&signing_key);
     if (ret != 0) {
@@ -12,6 +16,13 @@ int init_signing_key(void) {
     return wc_ed25519_import_public(SK_BYTES, sizeof(SK_BYTES), &signing_key);
 }
 
+/** @brief Verify a packet is signed with the encoder's signing key.
+ * 
+ *  @param packet: packet_t *, Pointer to the encrypted packet.
+ *  @param len: uint16_t, Length of the encrypted packet in bytes.
+ * 
+ *  @return int: 0 on success, -1 on failure.
+ */
 int verify_packet(packet_t * packet, uint16_t len) {
     int ret;
     int verified;
